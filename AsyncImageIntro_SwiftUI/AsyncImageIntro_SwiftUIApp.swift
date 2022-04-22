1e4127d9951d8bc2e73da7d9d277fb497fb76af9
Peyton Shetler <peyton.shetler@gmail.com>
2022-04-22 13:32:11 -0500

    Initial Commit

---
+++ AsyncImageIntro_SwiftUI/AsyncImageIntro_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageIntro_SwiftUIApp.swift
+//  AsyncImageIntro_SwiftUI
+//
+//  Created by Peyton Shetler on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageIntro_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
